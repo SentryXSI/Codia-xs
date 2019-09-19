e91150117a39a0cb9d87ec63de975b2b9ff14ff5
SentryXSI <34962312+SentryXSI@users.noreply.github.com>
2019-09-19 22:57:39 +1000

    Update .gitignore - add files

---
+++ app/kernel/Common.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Kernel;
+
+/**-----------------------------------------------------------------------------
+ *
+ * Class Common
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * @package App\Kernel
+ */
+class Common
+{
+    public function __construct( $options ) {
+
+    }
+
+    private function setup() {
+
+    }
+
+    private function session() {
+
+    }
+}
